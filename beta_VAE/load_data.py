b55872574dc3b26406176cf39406529f8b56f294
lidonghao <leedonhom@gmail.com>
2021-03-15 17:00:03 -0400

    new test

--- beta_VAE/load_data.py
+++ beta_VAE/load_data.py
@@ -5,7 +5,7 @@ import numpy as np
 def load_celeba(path):
     data = np.load(os.path.join(path, "data.npy"))
     data = data.astype(float)
-    data = data / 255.0 * 2.0 - 1.0  # -1 ~ 1
+    data = data / 255.0  # -1 ~ 1
     return data
 
 
