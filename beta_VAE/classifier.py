4530ff59e53fe1db398476275ab0e13af82462d8
lidonghao <leedonhom@gmail.com>
2021-03-28 19:35:35 -0400

    new test

--- beta_VAE/classifier.py
+++ beta_VAE/classifier.py
@@ -8,6 +8,13 @@ import tensorflow as tf
 (train_set, train_labels), (test_dataset, test_labels) = tf.keras.datasets.mnist.load_data()
 test_images = preprocess_images(test_dataset)
 classifier = Classifier(shape=(28, 28, 1))
+filePath = "./classifier"
+checkpoint_path = "./checkpoints/" + filePath
+ckpt = tf.train.Checkpoint(classifier=classifier)
+ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
+if ckpt_manager.latest_checkpoint:
+    ckpt.restore(ckpt_manager.latest_checkpoint)
+    print('classifier checkpoint restored!!')
 c_t = test_images
 c_l = test_labels
 for d in range(0, 180, 10):
@@ -19,10 +26,6 @@ classifier.compile(optimizer='adam',
                    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
                    metrics=['accuracy'])
 classifier.fit(c_t, c_l, epochs=50, verbose=1)
-filePath = "./classifier"
-checkpoint_path = "./checkpoints/" + filePath
-ckpt = tf.train.Checkpoint(classifier=classifier)
-ckpt_manager = tf.train.CheckpointManager(ckpt, checkpoint_path, max_to_keep=5)
 ckpt_save_path = ckpt_manager.save()
 print('Saving checkpoint for epoch {} at {}'.format(1,
                                                     ckpt_save_path))
