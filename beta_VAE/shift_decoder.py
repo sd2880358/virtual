76d45f607954f6153e8b01f6b54b425f5ed7c03e
lidonghao <leedonhom@gmail.com>
2021-05-15 17:31:31 -0400

    gi

--- beta_VAE/shift_decoder.py
+++ beta_VAE/shift_decoder.py
@@ -181,10 +181,10 @@ def start_train(epochs, model, s_decoder, full_range_set, partial_range_set, dat
                 d = np.radians(i)
                 r_x = rotate(test_sample, d)
                 ori_loss = compute_loss(model, test_sample)
-                rota_loss = reconstruction_loss(model, s_decoder, test_sample)
+                m_loss, rota_loss = reconstruction_loss(model, s_decoder, test_sample)
                 ori_cross_l = ori_cross_loss(model, s_decoder, test_sample, d, r_x)
                 rota_cross_l = rota_cross_loss(model, s_decoder, test_sample, d, r_x)
-                total_loss = ori_loss + rota_loss + ori_cross_l + rota_cross_l
+                total_loss = ori_loss + rota_loss + ori_cross_l + rota_cross_l + m_loss
                 loss(total_loss)
 
             elbo = -loss.result()
