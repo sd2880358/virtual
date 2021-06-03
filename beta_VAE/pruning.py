9abdb397b0dd90a2a38fd0169f7471ba0d9cc839
lidonghao <leedonhom@gmail.com>
2021-06-02 23:02:51 -0400

    gi

--- beta_VAE/pruning.py
+++ beta_VAE/pruning.py
@@ -51,7 +51,6 @@ def rotate_vector(vector, matrix):
 
 
 def start_train(epochs, teacher, full_range_set, partial_range_set, date, filePath):
-    @tf.function
     def train_step(teacher, model_for_pruning, train_x, degree_set, optimizer, step_callback):
         step_callback.on_train_batch_begin(batch=unused_arg)
         for i in range(10, degree_set, 10):
