a0f12e1fbd4d09383a185af154021901fa9df777
lidonghao <leedonhom@gmail.com>
2021-06-23 14:55:03 -0400

    gi

--- dataset/convert.py
+++ dataset/convert.py
@@ -3,5 +3,5 @@ import pandas as pd
 
 y, x = svm_read_problem('./australian_scale')
 features = [[i for i in x[j]] for j in range(len(x[0]))]
-data = pd.DataFrame([x, y], dtype='float32')
+data = pd.DataFrame([x, features], dtype='float32')
 data.to_csv("./c_australian_scale")
