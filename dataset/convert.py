249c4b2d913fac6e828db9d9ac16e6b7a32eb87b
lidonghao <leedonhom@gmail.com>
2021-06-23 19:31:08 -0400

    gi

--- dataset/convert.py
+++ dataset/convert.py
@@ -1,19 +1,16 @@
 from libsvm.svmutil import *
 import pandas as pd
 
-def convert_to_list(data):
-    l = []
+def convert_to_csv(data, labels, dic):
+    assert(len(data) == len(labels))
     for i in range(len(data)):
-        tmp = []
-        for key in data[i].keys():
-            tmp.append(data[i][key])
-        l.append(tmp)
-    return l
+        data[i]['labels'] = labels[i]
+    df = pd.DataFrame(data, dtype='float32')
+    df.to_csv('./{}'.format(dic), index=False)
 
 
 y, x = svm_read_problem('./australian_scale')
-features = convert_to_list(x)
-data = pd.DataFrame(features, dtype='float32')
-data.to_csv("./c_australian_scale", index=False)
+features = convert_to_csv(x, y, 'c_australian_scale')
+
 
 
