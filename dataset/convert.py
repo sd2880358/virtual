aea7534e42aa23ee5b4cdb91ef04b07bef072576
lidonghao <leedonhom@gmail.com>
2021-06-23 19:23:31 -0400

    gi

--- dataset/convert.py
+++ dataset/convert.py
@@ -2,6 +2,16 @@ from libsvm.svmutil import *
 import pandas as pd
 
 y, x = svm_read_problem('./australian_scale')
-features = [[i for i in x[z][j] for j in range(len(x[0]))] for z in range(len(x))]
+features = convert_to_list(x)
 data = pd.DataFrame(features, dtype='float32')
 data.to_csv("./c_australian_scale", index=False)
+
+
+def covert_to_list(data):
+    l = []
+    for i in range(len(data)):
+        tmp = []
+        for j in range(len(data[i])):
+            tmp.append(data[i][j])
+        l.append(tmp)
+    return l
